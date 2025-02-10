bf44b7b35e55682fd56def891978af40863f9188
Herick2D <contato.herickmoreira@gmail.com>
2025-02-10 17:03:30 -0300

    CHORE: configuração inicial do bot

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,40 @@
 import express from 'express';
+import multer from 'multer';
+import pdfParse from 'pdf-parse';
+import fs from 'fs/promises';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const upload = multer({ dest: 'uploads/' });
+
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send("OLÁAA");
 });
 
+app.post('/upload', upload.single('file'), async (req, res) => {
+  if (!req.file) {
+    return res.status(400).send('Nenhum arquivo enviado');
+  }
+
+  try {
+    const pdfBuffer = await fs.readFile(req.file.path);
+    const pdfData = await pdfParse(pdfBuffer);
+    console.log('Texto retirado do PDF:', pdfData.text);
+
+    await fs.unlink(req.file.path);
+
+    res.json({ text: pdfData.text });
+  } catch (error) {
+    console.error('Erro ao processar o arquivo:', error);
+    return res.status(500).send('Erro ao processar o arquivo');
+  }
+});
+
 app.listen(PORT, () => {
-  console.log('SERVER ESTÁ EM EXECUÇÃO')
+  console.log(`🚀 Server rodando em http://localhost:${PORT}`);
 });
